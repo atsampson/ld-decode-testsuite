5cec17058d7a917c38aa86b371fb386fc03096ca
Adam Sampson <ats@offog.org>
2023-02-14 23:59:51 +0000

    Add code to analyse filters at arbitrary frequencies.
    The FFT doesn't have much resolution at the low end, so I wanted to
    check that filters were actually behaving correctly there. (They are.)
    
    This could be used for plotting log frequency charts in the future...

--- filters/showfilter.py
+++ filters/showfilter.py
@@ -7,8 +7,8 @@ import scipy.interpolate as sint
 import scipy.signal as sps
 import matplotlib.pyplot as plt
 
-Filter = collections.namedtuple('Filter', ['name', 'b', 'a'],
-                                defaults=[[1.0]])
+Filter = collections.namedtuple('Filter', ['name', 'b', 'a', 'awkward'],
+                                defaults=[[1.0], False])
 Ideal = collections.namedtuple('Ideal', ['name', 'fs', 'vs', 'tols'],
                                defaults=[None])
 
@@ -28,6 +28,37 @@ def print_filter(fil):
         consts(fil.a)
         print('};')
 
+def analyse_filter(fil, fs, w):
+    h = np.zeros(len(w), dtype=complex)
+
+    for i, freq in enumerate(w):
+        # We can't do this for frequency 0
+        if freq < 1000:
+            h[i] = 0.0
+            continue
+
+        # Generate a cos waveform at frequency freq.
+        # We want at least 10 cycles, and at least 4 multiples of the filter
+        # length.
+        num_cycles = 10.0
+        num_samples = int((fs * num_cycles) / freq)
+        filter_len = 4 * max(len(fil.a), len(fil.b))
+        num_samples = max(num_samples, filter_len)
+        assert num_samples < 1e9
+
+        phases = (np.linspace(0.0, float(num_samples), num_samples)
+                  * 2.0 * np.pi / (fs / freq))
+        wave_in = 2.0 * np.cos(phases)
+
+        # Filter it
+        wave_out = sps.lfilter(fil.b, fil.a, wave_in)
+
+        # Detect it using a quadrature carrier
+        carrier = np.e ** (-1.0j * phases)
+        h[i] = np.sum(wave_out * carrier) / len(wave_out)
+
+    return w, h
+
 def show_filters(filters, fs, maxfreq=None, ideal_dbs=[], ideal_delays=[]):
     if maxfreq is None:
         maxfreq = fs / 2
@@ -40,7 +71,10 @@ def show_filters(filters, fs, maxfreq=None, ideal_dbs=[], ideal_delays=[]):
         print_filter(fil)
 
         w = np.linspace(0, maxfreq, POINTS)
-        w, h = sps.freqz(fil.b, fil.a, fs=fs, worN=w)
+        if fil.awkward:
+            w, h = analyse_filter(fil, fs, w)
+        else:
+            w, h = sps.freqz(fil.b, fil.a, fs=fs, worN=w)
         linax.plot(w, np.abs(h))
         logax.plot(w, 20 * np.log10(np.abs(h)))
         phaseax.plot(w, np.angle(h))
